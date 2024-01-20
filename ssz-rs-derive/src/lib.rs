5a8af6566a65d5151f04bc2640423019674b2a0c
bfan <ben@intrinsichtech.xyz>
2024-01-20 18:36:32 -0500

    test merkleproof for bitvector

--- ssz-rs-derive/src/lib.rs
+++ ssz-rs-derive/src/lib.rs
@@ -438,7 +438,6 @@ fn derive_merkle_proof_impl(data: &Data, name: &Ident, generics: &Generics) -> T
                 };
 
                 quote! {
-                    println!("index: {:?}", index);
                     if index == #i {
                         serde_json::to_value(&self.#field_name).unwrap()
                     }
